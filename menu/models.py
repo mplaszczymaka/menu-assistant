dfcfdd86740452be2edd93e6d77b4cc2a0b69e45
mplaszczymaka <plaszczyk16@wp.pl>
2020-10-28 23:59:11 +0100

    add models: Ingredient, Dish, Category, Menu

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    """ can be in many dishes """
+    name = models.CharField(max_length=30)
+    available = models.BooleanField(default=True)
+
+class Dish(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.SmallIntegerField()
+    ingredients = models.ManyToManyField(Ingredient)
+    amount = models.SmallIntegerField()
+    img = models.ImageField()
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    dishes = models.ManyToManyField(Dish)
+    description = models.CharField(max_length=200)
+
+class Menu(models.Model):
+    # there must be one main menu and may be few specials
+    name = models.CharField(max_length=60)
+    categories = models.ManyToManyField(Category)

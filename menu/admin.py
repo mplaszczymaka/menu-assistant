d8c7381a790f4f689bc1505a4b6784463f449a73
mplaszczymaka <plaszczyk16@wp.pl>
2020-10-29 00:19:36 +0100

    register models in admin site

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Ingredient, Dish, Category, Menu
 
 # Register your models here.
+admin.site.register([Ingredient, Dish, Category, Menu])
--- menu/models.py
+++ menu/models.py
@@ -6,19 +6,37 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=30)
     available = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.name
+
 class Dish(models.Model):
     name = models.CharField(max_length=50)
     price = models.SmallIntegerField()
     ingredients = models.ManyToManyField(Ingredient)
     amount = models.SmallIntegerField()
-    img = models.ImageField()
+    # img = models.ImageField()
+
+    class Meta:
+        verbose_name_plural = "Dishes"
+
+    def __str__(self):
+        return self.name
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
     dishes = models.ManyToManyField(Dish)
     description = models.CharField(max_length=200)
 
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
 class Menu(models.Model):
     # there must be one main menu and may be few specials
     name = models.CharField(max_length=60)
     categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name

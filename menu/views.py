9bed4842a1abab48811770b4fc1c3b447aa2ad98
mplaszczymaka <plaszczyk16@wp.pl>
2020-10-29 18:52:39 +0100

    include css, add index view and index template

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Menu
 
 # Create your views here.
+def index(request):
+    """ main page with logo, menu choice and account link in the future """
+    menus = Menu.objects.all()
+    
+    return render(request, 'menu/index.html', {'menus':menus})

8e2aa2e83f7e07c3f1184440462ac57f23f9fddf
mplaszczymaka <plaszczyk16@wp.pl>
2020-11-01 17:09:20 +0100

    if ingredient is not available then text is struck through

--- menu/models.py
+++ menu/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Ingredient(models.Model):
     """ can be in many dishes """
     name = models.CharField(max_length=30)
-    available = models.BooleanField(default=True)
+    is_available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name

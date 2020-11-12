4cbf2b4fefac4bbcc00cb5d4f6ced4065896568c
mplaszczymaka <plaszczyk16@wp.pl>
2020-11-12 16:39:23 +0100

    count and display dishes

--- menu/views.py
+++ menu/views.py
@@ -10,6 +10,16 @@ def index(request):
     return render(request, 'menu/index.html', {'menus':menus})
 
 def menu(request, menu_pk, category_pk, dish_pk = 0, quantity = 0):
+    """ all behavior of menu_view"""
+    # set current menu and its categories
+    menu = Menu.objects.get(pk=menu_pk) 
+    categories = menu.categories.all()
+    
+    # set current category to display dishes, default is first cat of specific menu
+    selected_category = Category.objects.get(pk=category_pk)
+    dishes = list(selected_category.dishes.all())
+
+    # add to basket if want
     if quantity == 1:
         dish = Dish.objects.get(pk=dish_pk)
         if Dish_in_basket.objects.filter(name=dish):
@@ -19,16 +29,16 @@ def menu(request, menu_pk, category_pk, dish_pk = 0, quantity = 0):
         else:
             Dish_in_basket.objects.create(name=dish, quantity=1)
 
-    menu = Menu.objects.get(pk=menu_pk) 
-    categories = menu.categories.all()
+    # count number of dishes in basket
+    count_dishes = []
+    for dish in Dish_in_basket.objects.all():
+        count_dishes.append(dish.quantity)
+    count_dishes = sum(count_dishes)
 
-    # set category to display dishes, default is first cat of specific menu
-    selected_category = Category.objects.get(pk=category_pk)
-    dishes = list(selected_category.dishes.all())
-
-    context =  {'menu'      :   menu,
-                'categories':categories,
-                'category' : selected_category,
-                'dishes': dishes,
+    context =  {'menu'        :   menu,
+                'categories'  :   categories,
+                'category'    :   selected_category,
+                'dishes'      :   dishes,
+                'count_dishes':   count_dishes,
                 }
     return render(request, 'menu/menu.html', context)

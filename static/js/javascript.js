793fb02e69df2828a60ace9444d1546151d197d6
mplaszczymaka <plaszczyk16@wp.pl>
2020-11-30 13:59:21 +0100

    similar button works with js

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -1,3 +1,12 @@
-$(".basket-btn").click(function(){
-    $(".basket").toggle();
-  });
+// (".basket-btn").click(function(){
+//     $(".basket").toggle();
+//   });
+
+function openSimilar() {
+    var el = document.getElementById('similar');
+    if (el.style.display == 'block') {
+        el.style.display = 'none';
+    } else {
+        el.style.display = 'block';
+    }
+}
